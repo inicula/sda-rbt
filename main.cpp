1e760b17d275d57e629070bdec6672b3f25e1665
Nicula Ionut <niculaionut@tutanota.com>
2021-05-27 23:11:54 +0300

    test cautare

--- main.cpp
+++ main.cpp
@@ -38,11 +38,20 @@ int main()
                 rbt.insert(key);
         }
 
+        // verifica daca valorile se gasesc in rbt
+        for(int key : random_keys)
+        {
+                const auto node = rbt.find(key);
+                assert(node != nullptr && node->key == key);
+        }
+
+        // verifica ordine corecta in parcurgerea inorder
         const std::vector<int> inorder_vec = rbt.get_inorder_vector();
         assert(std::is_sorted(inorder_vec.begin(), inorder_vec.end()));
 
         std::sort(random_keys.begin(), random_keys.end());
 
+        // verifica predecesori, succesori inorder
         for(unsigned i = 1; i < random_keys.size() - 1; ++i)
         {
                 const auto pred = rbt.inorder_predecessor(random_keys[i]);
