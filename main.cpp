1cfda9f878c70ac12016afdf9ecfb851c2c67deb
Nicula Ionut <niculaionut@tutanota.com>
2021-05-28 14:09:55 +0300

    check find time on sorted insertions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 #include <ctime>
 #include <random>
+#include <chrono>
+#include <set>
+#include <climits>
 
 void delete_duplicates(auto& vec)
 {
@@ -18,16 +21,18 @@ void delete_duplicates(auto& vec)
 
 int main()
 {
-        constexpr int size = 1 << 19;
+        constexpr int size = 1 << 22;
 
         RedBlackTree<int> rbt;
 
         std::vector<int> random_keys;
 
-        std::srand(std::time(0));
+        std::random_device rd;
+        std::mt19937 g(rd());
+        std::uniform_int_distribution<int> distrib(INT_MIN);
         for(int i = 0; i < size; ++i)
         {
-                const int value = std::rand();
+                const int value = distrib(g);
                 random_keys.push_back(value);
         }
 
@@ -64,4 +69,42 @@ int main()
         }
 
         std::cerr << "ok\n";
+
+        std::set<int> baseline;
+        RedBlackTree<int> rbt2;
+
+        // cheile sunt introduse in ordine crescatoare
+        for(const int key : random_keys)
+        {
+                baseline.insert(key);
+                rbt2.insert(key);
+        }
+
+        unsigned countbaseline = 0;
+
+        const auto t1 = std::chrono::high_resolution_clock::now();
+        for(const int key : random_keys)
+        {
+                countbaseline += baseline.contains(key);
+        }
+        const auto t2 = std::chrono::high_resolution_clock::now();
+
+        const auto dur1 = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+
+
+        unsigned countrbt = 0;
+
+        const auto t3 = std::chrono::high_resolution_clock::now();
+        for(const int key : random_keys)
+        {
+                countrbt += (rbt2.find(key) != nullptr);
+        }
+        const auto t4 = std::chrono::high_resolution_clock::now();
+
+        const auto dur2 = std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t3).count();
+
+        assert(countbaseline == countrbt);
+
+        std::cerr << "std::set<int> time: " << dur1 << '\n';
+        std::cerr << "RedBlackTree<int> time: " << dur2 << '\n';
 }
